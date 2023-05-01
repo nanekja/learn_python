58f41da927b0d761aff912ed605fb8e74f704c68
nanekja <raja.nannapaneni@gmail.com>
2023-05-01 17:18:40 +0530

    new file:   2_obj_attributes.py

---
+++ 2_obj_attributes.py
@@ -0,0 +1,12 @@
+# Every entity in Python is an object of a particular type
+# Every object has attributes some of which are methods
+
+# Here we see an integer is an object and it has attributes
+
+var = 5
+print(dir(var))
+
+# The names of the attributes that begin with double underscore are called 
+# private or magic attributes. They are usually used internally by Python itself.
+
+# Other attributes of an integer like numerator, real are attributes that return a value
